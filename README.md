bccf350889352cd1700cfb6e81dfe4f1922196c3
Joseph Venuto <jbvenuto@gmail.com>
2019-03-05 11:17:56 -0500

    Planned the logic of the app

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # northernlights
 Finds when the Northern Lights are most likely to be visible based on data from NOAA
+---
+## Abouth
+This application is designed to help hopeful aurora viewers know when to go out into the (often cold) night and look up.
---
+++ app.js
@@ -0,0 +1,20 @@
+var aurora = {
+    // Variables that will be used
+    userKp: "",
+    kpScores: [],
+
+    // Request to gather data from NOAA
+
+    
+    // Store the user's Kp
+
+
+    // Remove the already observed Kp scores from the array
+
+
+    // Check the estimated Kp scores compared to the user's Kp score
+
+    
+    // Return scores that are equal to or greater than user's Kp
+
+};
