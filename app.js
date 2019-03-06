6e0e8e4931f9fc3a451dfb6ad2908a2f2a254a5b
Joseph Venuto <jbvenuto@gmail.com>
2019-03-06 13:41:44 -0500

    Added fetch request to gather geomagnetic information

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
+var fetch = require('node-fetch');
+
 var aurora = {
     // Variables that will be used
-    userKp: "",
+    userKp: 4,
     kpScores: [],
+    auroraDates: [],
 
     // Request to gather data from NOAA
+    forcast: function() {
+        fetch('https://services.swpc.noaa.gov/products/noaa-planetary-k-index-forecast.json')
+        .then(res => res.json())
+        // .then(json => console.log(json))
+        .then(json => this.kpScores = json)
+        .then(() => this.findAurora());
+    },
 
-    
     // Store the user's Kp
-
-
-    // Remove the already observed Kp scores from the array
-
+    // (This function will be completed after app is functional)
+    
 
     // Check the estimated Kp scores compared to the user's Kp score
+    findAurora: function() {
+
+    }
 
     
     // Return scores that are equal to or greater than user's Kp
 
 };
+
+// Call the function to collect Kp data
+aurora.forcast();
