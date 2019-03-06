f000024b1c4e393e8078ca822e2fb1cb6133d69e
Joseph Venuto <jbvenuto@gmail.com>
2019-03-06 14:22:04 -0500

    Added function to sort through the data and find times when the Aurora will be visible

--- app.js
+++ app.js
@@ -17,11 +17,20 @@ var aurora = {
 
     // Store the user's Kp
     // (This function will be completed after app is functional)
-    
+
 
     // Check the estimated Kp scores compared to the user's Kp score
     findAurora: function() {
-
+        for (i = 0; i < this.kpScores.length; i++) {
+            if (this.kpScores[i][2] != 'observed' && parseInt(this.kpScores[i][1]) >= this.userKp) {
+                console.log(`Kp score of ${this.kpScores[i][1]} at ${this.kpScores[i][0]}`);
+                this.auroraDates.push({
+                    "date": this.kpScores[i][0],
+                    "kp": this.kpScores[i][1]
+                });
+            }           
+        }
+        console.log(this.auroraDates);
     }
 
     
